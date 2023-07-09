0851d8672e65d233affd82bd7382ebb5af0fb26f
DarthVanger <darthvanger@gmail.com>
2023-07-09 22:06:05 +0300

    Change canvas size from 100 to 95%

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 export const canvas = document.createElement('canvas')
 export const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth * 0.95
+canvas.height = window.innerHeight * 0.95
 
 document.body.append(canvas)
