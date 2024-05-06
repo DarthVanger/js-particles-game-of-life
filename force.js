9782c8fec9da1d9652061bdf9ebab0aef73a0d00
DarthVanger <darthvanger@gmail.com>
2024-05-06 17:44:21 +0300

    Refactor both forces into one
    Refactor universalPushForce and colorForce into one force function

--- force.js
+++ force.js
@@ -2,7 +2,7 @@ import { state } from './state.js'
 import { canvas, ctx } from './canvas.js'
 
 const universalPushForceRange = 20
-const colorForceRange = 100
+const colorForceRange = 200
 
 export function applyForce() {
   applyFriction()
@@ -26,31 +26,13 @@ function applyFriction() {
 }
 
 function applyParticleForce(p1, p2) {
-  applyAllParticleForces(p1, p2)
-}
-
-function applyAllParticleForces(p1, p2) {
     const force = getForceBetweenParticles(p1, p2)
     p2.vx += force.x
     p2.vy += force.y
 }
 
-function universalPushForce(p1, p2) {
-  const zeroForce = { x: 0, y: 0 }
-  const forceConstant = 1
-  const distX = p2.x - p1.x
-  const distY = p2.y - p1.y
-  const dist = Math.sqrt(distX * distX + distY * distY)
-  if (dist > universalPushForceRange * 2) return zeroForce;
-
-  const forceX = (distX / dist) * forceConstant / dist
-  const forceY = (distY / dist) * forceConstant / dist
-
-  return { x: forceX, y: forceY }
-}
-
-function colorForce(p1, p2) {
-  const zeroForce = { x: 0, y: 0 }
+function getForceBetweenParticles(p1, p2) {
+  const force = { x: 0, y: 0 }
 
   const forceMatrix = [
     [-1.5, -0.5, 0],
@@ -65,20 +47,17 @@ function colorForce(p1, p2) {
 
   const dist = Math.sqrt(distX * distX + distY * distY)
 
-  if (dist > colorForceRange * 2 || dist < universalPushForceRange) return zeroForce;
-
-  const forceX = (distX / dist) * forceConstant / dist
-  const forceY = (distY / dist) * forceConstant / dist
-
-  return { x: forceX, y: forceY }
-}
-
-function getForceBetweenParticles(p1, p2) {
-  const universalPushForceValue = universalPushForce(p1, p2)
-  const colorForceValue = colorForce(p1, p2)
-
-  return {
-    x: universalPushForceValue.x + colorForceValue.x,
-    y: universalPushForceValue.y + colorForceValue.y
+  if (dist < universalPushForceRange) {
+    force.x = (distX / dist) / dist
+    force.y = (distY / dist) / dist
+    return force
   }
+
+  if (dist < colorForceRange) {
+    force.x = (distX / dist) * forceConstant / dist
+    force.y = (distY / dist) * forceConstant / dist
+    return force
+  }
+
+  return force
 }
