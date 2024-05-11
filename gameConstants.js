7278568122c1c7bc6bf1738813765d0742e6de80
DarthVanger <darthvanger@gmail.com>
2024-05-12 01:14:34 +0300

    Reduce number of particles to 1000
    Also increase force range

--- gameConstants.js
+++ gameConstants.js
@@ -1,5 +1,5 @@
 export const particleRadius = 2
-export const universalPushForceRange = 10
-export const colorForceRange = 40
-export const numParticles = 2000
+export const universalPushForceRange = 20
+export const colorForceRange = 100
+export const numParticles = 1000
 
